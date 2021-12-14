6f5e9a951b8f6ee4190561524d27435fbe935000
ryanyee08 <ryanyee08@gmail.com>
2021-12-14 14:05:49 -0500

    Added Navmesh to Euclid Town
    Experimenting with Navmesh
    -Allowed player to click and move around map
    -Made buildings not walkthroughable

--- Assets/Scripts/IntroManager.cs
+++ Assets/Scripts/IntroManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// The sole purpose of intro manager is to help facillitate the new intro process. This should be made reuseable in the future
 public class IntroManager : MonoBehaviour
 {
     // Start is called before the first frame update
